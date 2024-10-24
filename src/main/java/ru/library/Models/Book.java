b2f0e75d83bf15c4e94cdc055d106832830d9696
Dmitrii Kravtsov <e4ahl1bhyr@gmail.com>
2024-10-24 16:40:22 +0300

    Implement book display

--- src/main/java/ru/library/Models/Book.java
+++ src/main/java/ru/library/Models/Book.java
@@ -1,7 +1,34 @@
 package ru.library.Models;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 public class Book extends Library{
     private int id;
+    @NotEmpty
+    private String title;
+    @NotEmpty
+    private String author;
+    @NotNull
+    private int year;
+    private boolean isTaken;
+
+    public Book() {}
+
+    public Book(String title, String author, int year, boolean isTaken) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isTaken = isTaken;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
     public int getId() {
         return id;
@@ -9,5 +36,38 @@ public class Book extends Library{
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public @NotEmpty String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(@NotEmpty String author) {
+        this.author = author;
+    }
+
+    @NotNull
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(@NotNull int year) {
+        this.year = year;
+    }
+
+    public boolean isTaken() {
+        return isTaken;
+    }
+
+    public void setTaken(boolean taken) {
+        isTaken = taken;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
     }
 }
--- src/main/java/ru/library/dao/BookDAO.java
+++ src/main/java/ru/library/dao/BookDAO.java
@@ -1,5 +1,21 @@
 package ru.library.dao;
 
-public class BookDAO {
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.library.Models.Book;
 
+import java.util.List;
+
+@Component
+public class BookDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    public BookDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Book> index() {
+        return jdbcTemplate.query("SELECT * FROM Book ORDER BY id", new BeanPropertyRowMapper<>(Book.class));
+    }
 }
